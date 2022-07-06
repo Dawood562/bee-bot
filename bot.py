3a9199a29c51b70d7f230f5834d1f7d90db895de
Dawood562 <dawooda0104@gmail.com>
2022-07-06 20:10:24 +0100

    Remove prints

--- bot.py
+++ bot.py
@@ -104,10 +104,8 @@ async def bees(ctx, msgid: int=None):
                 msg = f"This message has {vw} bees."
     else:
         vw = view_bees(msgid)
-        print(vw)
         if (vw == 1) or (vw.lower() == "a"):
             msg = f"This message has 1 bee."
-            print("1 bee")
         else:
             try:
                 vw = vw % 1
@@ -117,11 +115,8 @@ async def bees(ctx, msgid: int=None):
                 print(e)
             if vw == 0:
                 msg = f"This message has {int(vw)} bees."
-                print("vw % 1")
             else:
                 msg = f"This message has {vw} bees."
-                print("else")
-    print("send message")
     await ctx.reply(msg, mention_author=False)
 
 @client.command()
